67a166551d1a743af658f97ff1c6671fea8a217c
Mikkel Juul <mju@dmi.dk>
2021-07-13 12:27:55 +0200

    do not change keys' name
    add -- <ignore field keys> and a usage example

--- hdf5_json.py
+++ hdf5_json.py
@@ -1,13 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
 import numbers
 import sys
 
 import h5py
 import numpy as np
+
+ignore_fields = []
 
 
 # https://stackoverflow.com/questions/50916422/python-typeerror-object-of-type-int64-is-not-json-serializable/50916741
@@ -24,17 +22,16 @@ class NpEncoder(json.JSONEncoder):
 
 
 def unpack_group(g: h5py.Group) -> dict:
-    di = {k: decode(v) for k, v in dict(g.attrs).items()}
+    def attribute_values(attributes: dict) -> dict:  # attributes is a dict-like object
+        return {_k: decode(v) for _k, v in attributes.items() if _k not in ignore_fields}
+    di = attribute_values(dict(g.attrs))
     for k, grp in g.items():
+        if k in ignore_fields:
+            continue
         if isinstance(grp, h5py.Group):
-            if k.lower().startswith("data"):
-                groups = di.get("groups", [])
-                groups.append(unpack_group(grp))
-                di["groups"] = groups
-            else:
-                di[k] = unpack_group(grp)
+            di[k] = unpack_group(grp)
         elif isinstance(grp, h5py.Dataset):
-            di[k] = {k: decode(v) for k, v in dict(grp.attrs).items()}
+            di[k] = attribute_values(dict(grp.attrs))
     return di
 
 
@@ -47,9 +44,11 @@ def decode(anything):
         return str(anything)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    """
+    usage: python hdf5_json.py <some_h5_file> [-- <list of key names to ignore in the tree>]
+    """
     f = h5py.File(sys.argv[1], "r")
+    if len(sys.argv) > 3 and sys.argv[2] == "--":  # don't trigger if sys.argv[3] cannot be resolved < len is 3 not 2
+        ignore_fields = list(sys.argv[3:])
     print(json.dumps(unpack_group(f), indent=2, cls=NpEncoder))
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
